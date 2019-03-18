7c5de1bfd789addfbd52b723ad42304a32550cd4
zdlg168 <48328424+zdlg168@users.noreply.github.com>
2019-03-18 13:35:18 -0600

    Add files via upload

---
+++ Travel/Contact.php
@@ -0,0 +1,38 @@
+<?php
+	$title = "Travel Experts Website";
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+
+    <title><?php print($title); ?></title>
+		<style>
+		.center {
+			padding: 300px 0;
+			text-align: center;
+		}
+		</style>
+
+</head>
+<body>
+	<div class="center">
+		<div class="container TRVbackgroundWhite">	
+
+			<h3><p>Calgary Travel Agency: 403-686-5789</p>
+				<p>Edmondon Travel Agency: 780-896-9875</p>
+
+				<ul>
+	  			<li>North America Travel Agency: +1-604-569-9876</li>
+	  			<li>South America Travel Agency: +55-13-789-2164</li>
+	  			<li>Europe Travel Agency: +44-20-5788-4648 </li>
+			  	<li>Asia Travel Agency: +86-21-9869-6546</li>
+			  	<li>Africa Travel Agency: +27-11-978-5315</li>
+			  	<li>Australia Travel Agency: +61-03-7105-5486 </li>
+				</ul>
+		</div>
+</div>
+
+</body>
+</html>
