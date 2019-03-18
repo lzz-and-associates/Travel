75ba04d6557bc6adb387d56d69e213d19e4fc6c9
zdlg168 <48328424+zdlg168@users.noreply.github.com>
2019-03-18 13:35:45 -0600

    Add files via upload

---
+++ Travel/About.php
@@ -0,0 +1,28 @@
+<?php
+	$title = "Travel Experts Website";
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+
+    <title><?php print($title); ?></title>
+		<style>
+		.center {
+			padding: 300px 0;
+			text-align: center;
+		}
+		</style>
+
+</head>
+<body>
+  <div class="center">
+    <div class="container TRVbackgroundWhite">
+        <h3>About Us:</h3>
+          <h3><p>Local professional Travel Experts company for more than 40 years experience service in Calgary​</p></h3>
+    </div>
+  </div>
+
+</body>
+</html>
