fd027a64405c5c0bd51b09b0db604bd5e2f35456
zdlg168 <48328424+zdlg168@users.noreply.github.com>
2019-03-18 13:34:01 -0600

    Add files via upload

--- Travel/Main.php
+++ Travel/Main.php
@@ -1,44 +1,46 @@
-<!-- David Woods Work Begins -->
+
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<!-- Bootsrtap -->
-		<link 
-		rel="stylesheet" 
-		href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" 
-		integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" 
+		<link
+		rel="stylesheet"
+		href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+		integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
 		crossorigin="anonymous">
 		<link
 		rel = "stylesheet"
 		type = "text/css"
 		href = "CSS/Travel.css" />
-		
+
 		<!-- fontawesome
 		-->
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-		
-		
+
+
 		<meta charset='utf-8' />
 		<meta name='viewport' content='width=device-width, initial-scale=1' />
-	
+
 		<link rel='icon' href='JPLogo.png'>
-	
-		<title>Travel</title>
+
+		<title>Travel Experts Website</title>
 	</head>
 	<body id="TRVBackgroundImages" onload="TRVLoadingHide()">
 	<!-- --------------------------------------------------- -->
 	<!-- Nav Bar -->
 	<!-- --------------------------------------------------- -->
 	<?php
-		include('PHP/LoginBar.php');
+		include('PHP/LoginPage.php');
 		include('PHP/NavBar.php');
+
 	?>
 	<!-- --------------------------------------------------- -->
 	<!-- Nav Bar -->
 	<!-- --------------------------------------------------- -->
-		
-	
-	
+
+
+
 	<!-- --------------------------------------------------- -->
 	<!-- --------------------------------------------------- -->
 	<!-- Home -->
@@ -47,7 +49,7 @@
 	<div id="TRVHome" class="container">
 		<!-- Content -->
 		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
+			<?php
 				include('PHP/Placeholder.php');
 			?>
 		</div>
@@ -59,9 +61,9 @@
 	<!-- --------------------------------------------------- -->
 	<div id="TRVAbout" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
-				include('PHP/Placeholder.php');
+		<div class="cover2 container">
+			<?php
+				include('PHP/About.php');
 			?>
 		</div>
 	</div>
@@ -72,9 +74,9 @@
 	<!-- --------------------------------------------------- -->
 	<div id="TRVContact" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
-				include('PHP/Placehtrdsyhrtdsh.php');
+		<div class="cover2 container">
+			<?php
+				include('PHP/Contact.php');
 			?>
 		</div>
 	</div>
@@ -85,8 +87,8 @@
 	<!-- =================================================== -->
 	<div id="TRVrAfrica" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
+		<div class="cover2 container">
+			<?php
 				include('PHP/Placeholder.php');
 			?>
 		</div>
@@ -98,9 +100,9 @@
 	<!-- =================================================== -->
 	<div id="TRVrAsia" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
-				include('PHP/Placeholder.php');
+		<div class="cover2 container">
+			<?php
+
 			?>
 		</div>
 	</div>
@@ -111,8 +113,8 @@
 	<!-- =================================================== -->
 	<div id="TRVrAustraliaNewZealand" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
+		<div class="cover2 container">
+			<?php
 				include('PHP/Placeholder.php');
 			?>
 		</div>
@@ -124,8 +126,8 @@
 	<!-- =================================================== -->
 	<div id="TRVrEuropeUK" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
+		<div class="cover2 container">
+			<?php
 				include('PHP/Placeholder.php');
 			?>
 		</div>
@@ -137,8 +139,8 @@
 	<!-- =================================================== -->
 	<div id="TRVrMediterranean" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
+		<div class="cover2 container">
+			<?php
 				include('PHP/Placeholder.php');
 			?>
 		</div>
@@ -150,8 +152,8 @@
 	<!-- =================================================== -->
 	<div id="TRVrMiddleEast" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
+		<div class="cover2 container">
+			<?php
 				include('PHP/Placeholder.php');
 			?>
 		</div>
@@ -163,8 +165,8 @@
 	<!-- =================================================== -->
 	<div id="TRVrNorthAmerica" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
+		<div class="cover2 container">
+			<?php
 				include('PHP/Placeholder.php');
 			?>
 		</div>
@@ -176,8 +178,8 @@
 	<!-- =================================================== -->
 	<div id="TRVrSouthAmerica" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
+		<div class="cover2 container">
+			<?php
 				include('PHP/Placeholder.php');
 			?>
 		</div>
@@ -189,8 +191,8 @@
 	<!-- =================================================== -->
 	<div id="TRVrSouthPacific" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
+		<div class="cover2 container">
+			<?php
 				include('PHP/Placeholder.php');
 			?>
 		</div>
@@ -202,8 +204,8 @@
 	<!-- =================================================== -->
 	<div id="TRVrOther" class="container">
 		<!-- Content -->
-		<div class="cover2 container TRVbackgroundWhite">
-			<?php				
+		<div class="cover2 container">
+			<?php
 				include('PHP/Placeholder.php');
 			?>
 		</div>
@@ -217,25 +219,41 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	<!-- Bootsrtap -->
-	<script 
-	src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
-	integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+	<script
+	src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+	integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
 	crossorigin="anonymous">
 	</script>
-	<script 
-	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" 
-	integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" 
+	<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+	integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
 	crossorigin="anonymous">
 	</script>
-	<script 
-	src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" 
-	integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" 
+	<script
+	src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+	integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
 	crossorigin="anonymous">
 	</script>
-	
-	<script src="JS/ImageLibrary.js"></script> 
-	<script src="JS/Travel.js"></script> 
+
+	<script src="JS/ImageLibrary.js"></script>
+	<script src="JS/Travel.js"></script>
 	</body>
 </html>
-<!-- David Woods Work Ends -->
