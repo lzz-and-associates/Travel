57f1a3450a29a493e98e142f9f8736f0d031e579
LipovskiDavid <48325211+LipovskiDavid@users.noreply.github.com>
2019-03-18 13:43:53 -0600

    Delete Underconstruction.php

--- Travel/PHP/New folder/Underconstruction.php
+++
@@ -1,4 +0,0 @@
-		<div class='jumbotron'>
-		  <img class='warning' src='Images/ConStruct.jpg' alt='Contact' style='width:100%; height:100%;'>
-		  <div class='warning'>Under Construction</div>
-		</div>
